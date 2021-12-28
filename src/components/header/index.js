68588d649bc6105a71c55503ded4a6b7f0bff0a2
Jeremy Yonathan <jeremywib7@gmail.com>
2021-12-28 14:26:44 +0700

    first commit

---
+++ src/components/header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className={"header-container"}>
+            <div className="header">
+                <div className={"header-logo"}>
+                    Xpenser <i className="fi fi-rr-credit-card"></i>
+                </div>
+                <div className="header-button">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
